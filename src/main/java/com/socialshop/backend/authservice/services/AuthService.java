9da3c6c8a2361832d27e1ba3f5d60ee855637d98
Hung Nguyen <nxhung16102001@gmail.com>
2023-10-02 19:38:28 +0700

    [DEV] feature logout

--- src/main/java/com/socialshop/backend/authservice/services/AuthService.java
+++ src/main/java/com/socialshop/backend/authservice/services/AuthService.java
@@ -1,13 +1,11 @@
 package com.socialshop.backend.authservice.services;
 
 import com.socialshop.backend.authservice.constants.exception.BadRequestException;
-import com.socialshop.backend.authservice.services.dtos.LoginRequest;
-import com.socialshop.backend.authservice.services.dtos.RefreshTokenRequest;
-import com.socialshop.backend.authservice.services.dtos.RegisterRequest;
-import com.socialshop.backend.authservice.services.dtos.SessionAuthResponse;
+import com.socialshop.backend.authservice.services.dtos.*;
 
 public interface AuthService {
     SessionAuthResponse register(RegisterRequest request) throws BadRequestException;
     SessionAuthResponse login(LoginRequest request) throws BadRequestException;
+    void logout();
 
 }

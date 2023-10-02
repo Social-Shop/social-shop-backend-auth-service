9e6e063e782e21f953779a7a78828d04e2c42bf1
Hung Nguyen <nxhung16102001@gmail.com>
2023-10-02 16:15:27 +0700

    fix connection redis

--- src/main/java/com/socialshop/backend/authservice/configuration/RedisConfiguration.java
+++ src/main/java/com/socialshop/backend/authservice/configuration/RedisConfiguration.java
@@ -3,6 +3,7 @@ package com.socialshop.backend.authservice.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -10,7 +11,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 public class RedisConfiguration {
     @Bean
     public LettuceConnectionFactory lettuceConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(System.getenv("REDIS_HOST_URL"), Integer.parseInt(System.getenv("REDIS_HOST_PORT")));
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
